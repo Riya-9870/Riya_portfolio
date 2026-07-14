f9ac673ae114ffd30d77c298bac4d9816667e679
RIYA KUMARI <riyabhagat9870@gmail.com>
2026-07-14 16:12:57 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react],
+
+  // Change this to your GitHub repository name
+  base: "/Riya_portfolio/",
+});
